d24c65fcb9fa4f368e4daa2b94d541f65ad3832d
lynneeai <lynneeai@hotmail.com>
2015-06-15 14:32:30 -0400

    offers tests

--- CampaignManagerTester/InterceptorTester/Tests/CampaignManagerTests/Offers.cs
+++ CampaignManagerTester/InterceptorTester/Tests/CampaignManagerTests/Offers.cs
@@ -3,10 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+using System.IO;
+using System.Configuration;
+using Nito.AsyncEx;
+using System.IO.Compression;
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
+using ConsoleApplication1;
+using InterceptorTester.Tests.AdminTests;
 
 namespace InterceptorTester.Tests.CampaignManagerTests
 {
-    class Offers
+	[TestFixture()]
+	public class Offers
     {
+		[TestFixtureSetUp]
+		public void setup()
+		{
+			TestGlobals.setup ();
+		}
+
+		[Test()]
+		public static void getSpecialOffer()
+		{
+			string query = "/campaign-manager/Offers/" + "123123-123123-123123-1231312" + "?applicationKey=" + TestGlobals.applicationKey
+			               + "&sessionId=" + TestGlobals.sessionId;
+			GenericRequest getOffer = new GenericRequest (TestGlobals.campaignServer, query, null);
+			Test mTest = new Test (getOffer);
+			HttpClient client = new HttpClient ();
+			AsyncContext.Run (async () => await new HTTPSCalls ().runTest (mTest, HTTPOperation.GET, client));
+			string statusCode = HTTPSCalls.result.Key.GetValue ("StatusCode").ToString ();
+			Assert.AreEqual ("200", statusCode);
+				
+		}
+
+
+
     }
 }

e5e03a0d7e458ccfecb18afeb76d4aedd2bb82bc
lynneeai <lynneeai@hotmail.com>
2015-06-15 16:44:32 -0400

    double optIn

--- CampaignManagerTester/InterceptorTester/Tests/CampaignManagerTests/DoubleOptIn.cs
+++ CampaignManagerTester/InterceptorTester/Tests/CampaignManagerTests/DoubleOptIn.cs
@@ -3,10 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+using System.IO;
+using System.Configuration;
+using Nito.AsyncEx;
+using System.IO.Compression;
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
+using ConsoleApplication1;
+using InterceptorTester.Tests.AdminTests;
 
 namespace InterceptorTester.Tests.CampaignManagerTests
 {
-    class DoubleOptIn
+	[TestFixture()]
+	public class DoubleOptIn
     {
+		[TestFixtureSetUp()]
+		public void setup()
+		{
+			TestGlobals.setup ();
+		}
+
+		/*
+		[Test()]
+		public static void activateSignUp()
+		{
+			string query = "/campaign-manager/Signups/" + id + "/Activate";
+		}
+		*/
     }
 }

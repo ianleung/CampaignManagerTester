2922c96115e8cf7f1e7b808fb19ce567c55f6b22
kleril <kleril@users.noreply.github.com>
2015-06-15 12:06:07 -0400

    Added CampaignObject Tests
    NOTE: These tests are incomplete - API wasn't done, so the queries are
    probably wrong. PUT has no JSON attached to it, so do that when the API
    is fixed.

--- CampaignManagerTester/InterceptorTester/Tests/CampaignManagerTests/CampaignObject.cs
+++ CampaignManagerTester/InterceptorTester/Tests/CampaignManagerTests/CampaignObject.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ConsoleApplication1;
+using NUnit.Framework;
+using Nito.AsyncEx;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,65 @@ using System.Threading.Tasks;
 
 namespace InterceptorTester.Tests.CampaignManagerTests
 {
+    [TestFixture()]
     class CampaignObject
     {
+        [TestFixtureSetUp()]
+        public void setup()
+        {
+            TestGlobals.setup();
+        }
+
+        [Test()]
+        public static void getHappyPath()
+        {
+            //Setup strings
+            string applicationKey = TestGlobals.applicationKey;
+            string sessionId = TestGlobals.sessionId;
+
+            GenericRequest request = new GenericRequest(TestGlobals.adminServer, "/campaign-manager/Campaigns?"
+            + "applicationKey=" + applicationKey + "&"
+            + "sessionId=" + sessionId, null);
+
+            Test mTest = new Test(request);
+            AsyncContext.Run(async () => await new HTTPSCalls().runTest(mTest, HTTPOperation.GET));
+            Console.WriteLine(HTTPSCalls.result.Value.ToString());
+            Assert.AreEqual("200", HTTPSCalls.result.Value.ToString());
+        }
+
+        //TODO: Do this when API is legible
+        [Test()]
+        public static void putHappyPath()
+        {
+            //Setup strings
+            string applicationKey = TestGlobals.applicationKey;
+            string sessionId = TestGlobals.sessionId;
+
+            GenericRequest request = new GenericRequest(TestGlobals.adminServer, "/campaign-manager/Campaigns?"
+            + "applicationKey=" + applicationKey + "&"
+            + "sessionId=" + sessionId, null);
+
+            Test mTest = new Test(request);
+            AsyncContext.Run(async () => await new HTTPSCalls().runTest(mTest, HTTPOperation.PUT));
+            Console.WriteLine(HTTPSCalls.result.Value.ToString());
+            Assert.AreEqual("200", HTTPSCalls.result.Value.ToString());
+        }
+
+        [Test()]
+        public static void deleteHappyPath()
+        {
+            //Setup strings
+            string applicationKey = TestGlobals.applicationKey;
+            string sessionId = TestGlobals.sessionId;
+
+            GenericRequest request = new GenericRequest(TestGlobals.adminServer, "/campaign-manager/Campaigns?"
+            + "applicationKey=" + applicationKey + "&"
+            + "sessionId=" + sessionId, null);
+
+            Test mTest = new Test(request);
+            AsyncContext.Run(async () => await new HTTPSCalls().runTest(mTest, HTTPOperation.DELETE));
+            Console.WriteLine(HTTPSCalls.result.Value.ToString());
+            Assert.AreEqual("204", HTTPSCalls.result.Value.ToString());
+        }
     }
 }

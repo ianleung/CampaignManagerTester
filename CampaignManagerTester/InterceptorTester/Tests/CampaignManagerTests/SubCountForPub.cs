792aa91e1f3f2d7a6edd20c90dfc78edb27917a2
lynneeai <lynneeai@hotmail.com>
2015-06-23 12:06:50 -0400

    test names

--- CampaignManagerTester/InterceptorTester/Tests/CampaignManagerTests/SubCountForPub.cs
+++ CampaignManagerTester/InterceptorTester/Tests/CampaignManagerTests/SubCountForPub.cs
@@ -19,7 +19,7 @@ namespace InterceptorTester.Tests.CampaignManagerTests
         }
 
         [Test()]
-        public static void happyPath()
+        public static void subCountForPubHappyPath()
         {
             //Setup strings
             string applicationKey = TestGlobals.applicationKey;
